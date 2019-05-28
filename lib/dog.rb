137146aa24aa4764fb251cba01be5429ed887a68
Kishandth Sivapalasundaram <rodes88@msn.com>
2019-05-28 19:11:47 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,15 +1,6 @@
 require 'pry'
 
 class Dog < ActiveRecord::Base
-  attr_accessor :name, :breed
   
-  
-  def self.update(x)
-    binding.pry
-  end 
-  
-  def self.find_by_name(name)
-    #self.find_by(name: name)
-  end 
   
 end
