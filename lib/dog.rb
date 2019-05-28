9d709312c0a32961dbc111df2242251c302e2ef3
Kishandth Sivapalasundaram <rodes88@msn.com>
2019-05-28 18:39:15 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,7 +1,7 @@
 class Dog < ActiveRecord::Base
   attr_accessor :name, :breed
   
-  def self.find_by_name(x={})
-    self.find_by(x)
+  def self.find_by_name(name:)
+    self.find_by(name)
   end 
 end
